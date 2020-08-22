a448023930a032b340fa2d8a364a97252c5ef69d
Munish Jadhav <monish_jad@yahoo.com>
2020-08-22 20:41:48 +0100

    ReactClasswork/Extra/ReactRouter/Class4

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from "react";
-import Logo from './Logo.js';
-import BestPokemonSelector from './BestPokemon.js';
-import CaughtPokemon from './CaughtPokemon.js';
-
-
- 
-
-    
-    
-
+import Logo from "./Logo.js";
+import BestPokemonSelector from "./BestPokemon.js";
+import CaughtPokemon from "./CaughtPokemon.js";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
 function App() {
-  function logWhenClicked () {
+  function logWhenClicked() {
     console.log("Image was clicked");
+  }
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/best-pokemon">Best Pokemon</Link>
+        <Link to="/caught-pokemon">Caught Pokemon</Link>
+      </div>
+      
+      <div>
+        <Logo handleClick={logWhenClicked} appName="Pokedex" />
+        {/* <BestPokemonFetcher /> */}
+        <Switch>
+        <Route path="/best-pokemon" component={BestPokemonSelector} />
+      </Switch>
+      <Switch>
+        {/* <Route path="/caught-pokemon" component={CaughtPokemon} /> */}
+        <Route path="/caught-pokemon" render={() => <CaughtPokemon date={new Date().toLocaleDateString()} />}/>
+      </Switch>
+        {/* <BestPokemonSelector /> */}
+        {/* <CaughtPokemon date={new Date().toLocaleDateString()} /> */}
+      </div>
+    </BrowserRouter>
+  );
 }
-  return(
-    
-<div>
-
-<Logo handleClick={logWhenClicked} appName = "Pokedex"/>
-{/* <BestPokemonFetcher /> */}
-<BestPokemonSelector />
-<CaughtPokemon date = {new Date().toLocaleDateString()}/>
-</div>
-)}
-
-
-
-
-
 
 export default App;
